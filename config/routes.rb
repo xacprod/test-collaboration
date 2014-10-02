0ca00e81e0558f9e31972141908b61f4f6c24e62
Иван Бишевац <ivan.bisevac@gmail.com>
2014-10-02 20:26:42 +0200

    Added registration/login gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  get 'static_pages/welcome', to: 'static_pages#welcome'
+
+  root to: 'static_pages#welcome'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
